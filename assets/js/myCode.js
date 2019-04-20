7f33a196bea6e26fae665d31f7184a1546e42581
hayatzsala <hayatzs.4ever@gmail.com>
2019-04-20 04:40:54 +0300

    final edit in profile admin

--- assets/js/myCode.js
+++ assets/js/myCode.js
@@ -1,3 +1,125 @@
-$('#contTable').click(function(){
- $("#Tables").slideToggle("slow");
-});
+  $(document).ready(function(){
+        $("#home-tapped").click(function(){
+        $("#home").css("display", "block");
+        $("#email").css("display","none");
+        $("#Users").css("display","none");
+        $("#Admins").css("display","none");
+        $("#Tours").css("display","none");
+        $("#Hotels").css("display","none");
+        $("#Flights").css("display","none");
+        $("#Cars").css("display","none");
+        $("#Profile").css("display","none");
+
+            });
+        $("#Email-tapped").click(function(){
+        $("#home").css("display", "none");
+        $("#email").css("display","block");
+        $("#Users").css("display","none");
+        $("#Admins").css("display","none");
+        $("#Tours").css("display","none");
+        $("#Hotels").css("display","none");
+        $("#Flights").css("display","none");
+        $("#Cars").css("display","none");
+        $("#Profile").css("display","none");
+            });
+
+         $("#Users-tapped").click(function(){
+        $("#home").css("display", "none");
+        $("#email").css("display","none");
+        $("#Users").css("display","block");
+        $("#Admins").css("display","none");
+        $("#Tours").css("display","none");
+        $("#Hotels").css("display","none");
+        $("#Flights").css("display","none");
+        $("#Cars").css("display","none");
+        $("#Profile").css("display","none");
+            });
+
+          $("#Admins-tapped").click(function(){
+        $("#home").css("display", "none");
+        $("#email").css("display","none");
+        $("#Users").css("display","none");
+        $("#Admins").css("display","block");
+        $("#Tours").css("display","none");
+        $("#Hotels").css("display","none");
+        $("#Flights").css("display","none");
+        $("#Cars").css("display","none");
+        $("#Profile").css("display","none");
+            });
+
+           $("#Tours-tapped").click(function(){
+        $("#home").css("display", "none");
+        $("#email").css("display","none");
+        $("#Users").css("display","none");
+        $("#Admins").css("display","none");
+        $("#Tours").css("display","block");
+        $("#Hotels").css("display","none");
+        $("#Flights").css("display","none");
+        $("#Cars").css("display","none");
+        $("#Profile").css("display","none");
+            });
+
+            $("#Hotels-tapped").click(function(){
+        $("#home").css("display", "none");
+        $("#email").css("display","none");
+        $("#Users").css("display","none");
+        $("#Admins").css("display","none");
+        $("#Tours").css("display","none");
+        $("#Hotels").css("display","block");
+        $("#Flights").css("display","none");
+        $("#Cars").css("display","none");
+        $("#Profile").css("display","none");
+            });
+
+             $("#Flights-tapped").click(function(){
+        $("#home").css("display", "none");
+        $("#email").css("display","none");
+        $("#Users").css("display","none");
+        $("#Admins").css("display","none");
+        $("#Tours").css("display","none");
+        $("#Hotels").css("display","none");
+        $("#Flights").css("display","block");
+        $("#Cars").css("display","none");
+        $("#Profile").css("display","none");
+            });
+
+         $("#Cars-tapped").click(function(){
+        $("#home").css("display", "none");
+        $("#email").css("display","none");
+        $("#Users").css("display","none");
+        $("#Admins").css("display","none");
+        $("#Tours").css("display","none");
+        $("#Hotels").css("display","none");
+        $("#Flights").css("display","none");
+        $("#Cars").css("display","block");
+        $("#Profile").css("display","none");
+            });
+
+         $("#profile-tapped").click(function(){
+        $("#home").css("display", "none");
+        $("#email").css("display","none");
+        $("#Users").css("display","none");
+        $("#Admins").css("display","none");
+        $("#Tours").css("display","none");
+        $("#Hotels").css("display","none");
+        $("#Flights").css("display","none");
+        $("#Cars").css("display","none");
+        $("#Profile").css("display","block");
+            });
+
+        
+         $("#edit-button").click(function(){
+        $("#home").css("display", "none");
+        $("#email").css("display","none");
+        $("#Users").css("display","none");
+        $("#Admins").css("display","none");
+        $("#Tours").css("display","none");
+        $("#Hotels").css("display","none");
+        $("#Flights").css("display","none");
+        $("#Cars").css("display","none");
+        $("#Profile").css("display","block");
+            });
+         $('#Reset').click(function(){
+            $('#password-section').css("display","block");
+         });
+     });
